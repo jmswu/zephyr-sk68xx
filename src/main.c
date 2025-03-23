0b216a2fdcedfb03d085a7d234f49553837361b6
James Wu <j.wu@mstglobal.com>
2025-03-23 11:40:52 +1000

    working LED

--- src/main.c
+++ src/main.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+#define LED_NODE DT_ALIAS(led0)
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
 int main(void)
 {
 
+    int ret;
+    if (!gpio_is_ready_dt(&led))
+    {
+        printk("GPIO not ready\n");
+        return 0;
+    }
+
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0)
+    {
+        printk("Can't config pin\n");
+        return 0;
+    }
+
     printk("Hello world\n");
+
+    for (;;)
+    {
+        gpio_pin_set_dt(&led, 1);
+        k_msleep(100);
+        gpio_pin_set_dt(&led, 0);
+        k_msleep(100);
+    }
 
     return 0;
 }
